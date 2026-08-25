a7e1fd267d4192a9f41b2156cddf95cb639b5388
Rocha0101 <paloma.santos.ps468@gmail.com>
2026-08-25 20:52:27 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+//Importa todo o CSS do Bootstrap para o projeto
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+//Importa estilos personalizados do projeto
 import './style.css';
 
 console.log('Bootstrap e Vite carregados com sucesso');
